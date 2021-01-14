60cce1337ac8f8bff32031bbf6b0540407f4cbb2
Aboubakr Mostafa <66839270+Aboubakr-m@users.noreply.github.com>
2021-01-14 18:39:43 +0200

    Create 231A - Team.java

---
+++ 231A - Team.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Sol {
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());  
+        int x = 0;
+        for(int i = 0;i < n; i++){
+            String lines = br.readLine();
+            String[] str = lines.trim().split("\\s+");
+            int arr[] = new int[str.length];
+            for (int j=0;j <str.length;j++){
+                arr[j] = Integer.parseInt(str[j]);
+            }
+            if (arr[0] + arr[1] + arr[2] >= 2)
+                x++;
+        }       
+        System.out.println(x);
+    }
