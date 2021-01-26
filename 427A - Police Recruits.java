2f0824997f90dcdfa0ed82a4da5703ba36af5906
Aboubakr Mostafa <66839270+Aboubakr-m@users.noreply.github.com>
2021-01-26 04:25:16 +0200

    Update 427A - Police Recruits.java

--- 427A - Police Recruits.java
+++ 427A - Police Recruits.java
@@ -4,13 +4,13 @@ public class Sol {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        int n = Integer.parseInt(br.readLine());  
        String line = br.readLine();
+       int untreated = 0, free = 0;
        String strs[] = line.trim().split("\\s+");
        int a[] = new int[strs.length];
        for (int i = 0; i < strs.length; i++){
            a[i] = Integer.parseInt(strs[i]);
        }
        //.........................................
-       int untreated = 0, free = 0;
        for (int i = 0; i < a.length; i++){
            if (a[i] > 0){
                 free += a[i];
