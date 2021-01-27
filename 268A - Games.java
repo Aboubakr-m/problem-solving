21a1ca69676bf00e6d7d5e4cd3d9649aec14e570
Aboubakr Mostafa <66839270+Aboubakr-m@users.noreply.github.com>
2021-01-27 16:07:10 +0200

    Create 268A - Games.java

---
+++ 268A - Games.java
@@ -0,0 +1,32 @@
+    import java.util.Scanner;
+    public class Problem1 {
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int a[] = new int[n*2];
+            int b[] = new int[n];
+            int c[] = new int[n];
+            int games = 0;
+            for (int i = 0; i < n*2; i++){
+                a[i] = in.nextInt();
+            }
+            int x = 0, y = 0;
+            for (int i = 0; i < a.length; i++){
+                if (i % 2 == 0){
+                    b[x] = a[i];
+                    x++;
+                }
+                else {
+                    c[y] = a[i];
+                    y++;
+                }
+            }
+            for (int i = 0; i < b.length; i++){
+                for (int j = 0; j < c.length; j++){
+                    if (b[i] == c[j])
+                        games++;
+                }
+            }
+            System.out.println(games);
+        }
+    }
