85bd6895e5c5541d163a009d0780ed1a9473101a
Aboubakr Mostafa <66839270+Aboubakr-m@users.noreply.github.com>
2021-04-04 23:44:11 +0200

    Update 443A - Anton and Letters.java

--- 443A - Anton and Letters.java
+++ 443A - Anton and Letters.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class sol {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<Character> data = new HashSet<Character>();
+        Set<Character> data = new HashSet<>();
         String line = br.readLine();
         for (int i = 0; i < line.length(); i++) {
             if (line.charAt(i) > 96 && line.charAt(i) < 123)
