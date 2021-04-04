1df2d058dbd6d7a29f8a99e5ac65bfffea8b508b
Aboubakr Mostafa <66839270+Aboubakr-m@users.noreply.github.com>
2021-04-04 23:41:23 +0200

    Create 443A - Anton and Letters.java

---
+++ 443A - Anton and Letters.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class sol {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Character> data = new HashSet<Character>();
+        String line = br.readLine();
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) > 96 && line.charAt(i) < 123)
+                data.add(line.charAt(i));
+        }
+        System.out.println(data.size());
+    }
+}
