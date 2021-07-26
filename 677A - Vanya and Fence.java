3e62d71cdfc3c00eddd50f915e6cb938a1a6d2d9
Aboubakr Mostafa <abobakrmostafa96@gmail.com>
2021-07-26 10:25:06 +0200

    Update 677A - Vanya and Fence.java

--- 677A - Vanya and Fence.java
+++ 677A - Vanya and Fence.java
@@ -1,19 +1,19 @@
-    import java.util.*;
-    public class sol {
-        public static void main(String[] args){
-            Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            int h = in.nextInt();
-            int a, width = 0;
-            for (int i = 0;i < n;i++){
-                a = in.nextInt();
-                if (a > h){
-                    width += 2;
-                }
-                else{
-                    width++;
-                }
+import java.util.*;
+public class sol {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int h = in.nextInt();
+        int a, width = 0;
+        for (int i = 0;i < n;i++){
+            a = in.nextInt();
+            if (a > h){
+                width += 2;
             }
-            System.out.println(width);
+            else{
+                width++;
+            }
         }
+        System.out.println(width);
     }
+}
